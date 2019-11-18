6293dca57c1e6a1c6dfaba9f3c2f26b67cbb9dcd
IgorKey <Igor_Kuzminykh@epam.com>
2019-11-18 11:14:36 +0300

    update to 0.4.0

--- src/main/kotlin/com/epam/drill/version/VersionRetriever.kt
+++ src/main/kotlin/com/epam/drill/version/VersionRetriever.kt
@@ -14,48 +14,26 @@ import java.util.concurrent.TimeUnit
 
 
 class VersionRetriever : Plugin<Project> {
-    var tagDistance: Int = 0
-    private val tagVersionRegex = Regex("refs/tags/v(\\d+)\\.(\\d+)\\.(\\d+)")
+    private val tagVersionRegex = Regex("refs/tags/(\\d+)\\.(\\d+)\\.(\\d+)")
 
     override fun apply(target: Project) {
         target.run {
             val git = Git.wrap(FileRepository(target.rootProject.rootDir.resolve(".git")))
             try {
-
                 val allTags = git.tagList().call()
+                allTags.forEach {
+                    logger.info(it.name)
+                }
+
                 val lastTag = allTags.sortedBy { it.getSnapShot().lastModifiedInstant().toEpochMilli() }.lastOrNull { tagVersionRegex.matches(it.name) }
+                logger.info(lastTag?.name)
                 if (lastTag != null) {
                     val (_, major, minor, patch) = tagVersionRegex.matchEntire(lastTag.name)!!.groupValues
-                    tagDistance = git.getTagDistance(lastTag)
-                    target.version = when (tagDistance) {
-                        0 -> "$major.$minor.$patch"
-                        else -> "$major.$minor.${patch.toInt().inc()}"
-                    }
-                } else target.version = "0.3.20"
+                    target.version = "$major.$minor.$patch"
+                } else target.version = "0.1.0"
             } catch (ex: Exception) {
-                println("set the default tag. Exception occured")
-                target.version = "0.3.20"
-            }
-            tasks {
-                register("incrementVersion") {
-                    val githubUserName = if (project.hasProperty("githubUserName"))
-                        project.property("githubUserName").toString()
-                    else System.getenv("githubUserName") ?: null
-                    if (githubUserName == null) {
-                        println("please provide GH credentials")
-                        return@register
-                    }
-                    if (tagDistance > 0) {
-                        println(tagDistance)
-                        println(target.version)
-                        git.tag("v${target.version}")
-
-                        val githubUserPassword = if (project.hasProperty("githubUserPassword"))
-                            project.property("githubUserPassword").toString()
-                        else System.getenv("githubUserPassword")
-                        git.push().setPushTags().setCredentialsProvider(UsernamePasswordCredentialsProvider(githubUserName, githubUserPassword)).call()
-                    }
-                }
+                logger.error("xx", ex)
+                target.version = "0.1.0"
             }
         }
     }
