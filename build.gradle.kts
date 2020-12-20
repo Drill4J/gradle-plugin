f01734effa56d605911ddd31bd8b1ffd37cdc0ae
Viktor Bellavin <viktor_bellavin@epam.com>
2020-12-21 01:15:47 +0300

    EPMDJ-5743 Removed version plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
-    `kotlin-dsl`
+    `kotlin-dsl-base`
     `java-gradle-plugin`
     `maven-publish`
 }
 
 val scriptUrl: String by extra
+
+val kotlinVersion: String by extra
 
 apply(from = "$scriptUrl/git-version.gradle.kts")
 
@@ -14,44 +16,12 @@ repositories {
     jcenter()
 }
 
-val kotlinVersion: String by extra
-val semverVersion: String by extra
-
 dependencies {
-    implementation(gradleApi())
-    implementation(kotlin("stdlib-jdk8", kotlinVersion))
-    implementation(kotlin("gradle-plugin", kotlinVersion))
-    implementation("com.epam.drill:semver:$semverVersion")
-
-    testImplementation(kotlin("test", kotlinVersion))
-    testImplementation(kotlin("test-junit", kotlinVersion))
-    testImplementation("org.eclipse.jgit:org.eclipse.jgit:5.5.0.201909110433-r")
-    testImplementation(gradleTestKit())
-}
-
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    test {
-        systemProperty("project.version", project.version)
-        dependsOn(publishToMavenLocal)
-    }
+    compileOnly(kotlin("gradle-plugin", kotlinVersion))
 }
 
 gradlePlugin {
     plugins {
-        create("version") {
-            id = "$group.version"
-            implementationClass = "com.epam.drill.gradle.VersionRetriever"
-
-        }
-        create("version.plugin") {
-            id = "$group.version.plugin"
-            implementationClass = "com.epam.drill.gradle.VersionRetriever"
-
-        }
         create("cross-compilation") {
             id = "$group.cross-compilation"
             implementationClass = "com.epam.drill.gradle.CrossCompilation"
